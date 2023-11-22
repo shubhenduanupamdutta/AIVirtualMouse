35fb278ccc903a1b5408695dbeb3c015d0992836
ShubhenduADutta <shubhendu@flexli.in>
2023-11-22 09:11:12 +0530

    Add find_distance method to HandDetector class.

--- hand_tracking_module.py
+++ hand_tracking_module.py
@@ -1,6 +1,7 @@
 """
 OpenCV Hand Tracking Min
 """
+import math
 
 import cv2 as cv
 import mediapipe as mp
@@ -8,28 +9,34 @@ import time
 
 
 class HandDetector:
-    def __init__(self, mode: bool = False, max_hands: int = 2,
-                 model_complexity: float = 1,
-                 detection_confidence: float = 0.5,
-                 track_confidence: float = 0.5):
+    def __init__(
+        self,
+        mode: bool = False,
+        max_hands: int = 2,
+        model_complexity: float = 1,
+        detection_confidence: float = 0.5,
+        track_confidence: float = 0.5,
+    ):
         self.mode = mode
         self.max = max_hands
         self.detection_confidence = detection_confidence
         self.track_confidence = track_confidence
         self.model_complexity = model_complexity
         self.mpHands = mp.solutions.hands  # type: ignore
-        self.hands = self.mpHands.Hands(self.mode, self.max,
-                                        self.model_complexity,
-                                        self.detection_confidence,
-                                        self.track_confidence)
+        self.hands = self.mpHands.Hands(
+            self.mode,
+            self.max,
+            self.model_complexity,
+            self.detection_confidence,
+            self.track_confidence,
+        )
         self.mpDraw = mp.solutions.drawing_utils  # type: ignore
         self.results = None
         self.tip_ids = [4, 8, 12, 16, 20]
         self.landmarks = None
 
     def find_hands(self, img, draw: bool = True):
-        drawing_spec = self.mpDraw.DrawingSpec(
-            color=(0, 255, 0), thickness=1, circle_radius=1)
+        drawing_spec = self.mpDraw.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=1)
         img_RGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         self.results = self.hands.process(img_RGB)
         # print(results.multi_hand_landmarks)
@@ -41,7 +48,7 @@ class HandDetector:
                         handLandmarks,
                         self.mpHands.HAND_CONNECTIONS,
                         landmark_drawing_spec=drawing_spec,
-                        connection_drawing_spec=drawing_spec
+                        connection_drawing_spec=drawing_spec,
                     )
         return img
 
@@ -71,6 +78,20 @@ class HandDetector:
 
         return fingers
 
+    def find_distance(self, p1: int, p2: int, image, draw: bool = True, r: int = 15, t: int = 3):
+        x1, y1 = self.landmarks[p1][1:]
+        x2, y2 = self.landmarks[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv.line(image, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv.circle(image, (x1, y1), r, (255, 0, 255), cv.FILLED)
+            cv.circle(image, (x2, y2), r, (255, 0, 255), cv.FILLED)
+            cv.circle(image, (cx, cy), r, (0, 0, 255), cv.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        return length, image, [x1, y1, x2, y2, cx, cy]
+
 
 def main():
     previous_time = 0
@@ -88,13 +109,12 @@ def main():
         fps = 1 / (current_time - previous_time)
         previous_time = current_time
 
-        cv.putText(img, str(int(fps)), (10, 70),
-                   cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        cv.putText(img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
         cv.imshow("Image", img)
         key = cv.waitKey(1)
         if key == ord("q"):
             break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
